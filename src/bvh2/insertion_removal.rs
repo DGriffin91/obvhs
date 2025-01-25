a0a469bf80c84c62e5c6de0a2f4d468be9c87c77
DGriffin91 <github@dgdigital.net>
2025-01-25 13:14:45 -0800

    don't fully recompute inherited_cost for each cand

--- src/bvh2/insertion_removal.rs
+++ src/bvh2/insertion_removal.rs
@@ -17,6 +17,12 @@ pub fn remove_node(bvh: &mut Bvh2, node_id: usize, parents: &[u32]) {
     bvh.refit_from_fast(parent_id, &parents);
 }
 
+#[derive(Debug, Default, Clone, Copy)]
+pub struct SiblingCandidate {
+    inherited_cost: f32,
+    index: u32,
+}
+
 /// Searches through tree recursively to find the best sibling for the node being inserted. The best sibling is
 /// classified as the sibling that if chosen would increase the surface area of the BVH the least.
 /// When the best sibling is found, a parent of both the sibling and the new node in put in the location of
@@ -26,7 +32,7 @@ pub fn insert_leaf_node(
     bvh: &mut Bvh2,
     new_node: Bvh2Node,
     parents: &mut Vec<u32>,
-    stack: &mut HeapStack<u32>,
+    stack: &mut HeapStack<SiblingCandidate>,
 ) {
     debug_assert!(new_node.is_leaf());
     let mut min_cost = f32::MAX;
@@ -34,49 +40,36 @@ pub fn insert_leaf_node(
 
     stack.clear();
 
+    let root_aabb = bvh.nodes[0].aabb;
+
     // Traverse the BVH to find the best sibling
-    stack.push(0);
-    while let Some(current_node_index) = stack.pop() {
-        let current_node_index = *current_node_index as usize;
+    stack.push(SiblingCandidate {
+        inherited_cost: root_aabb.union(&new_node.aabb).half_area() - root_aabb.half_area(),
+        index: 0,
+    });
+    while let Some(sibling_candidate) = stack.pop() {
+        let current_node_index = sibling_candidate.index as usize;
 
         let candidate = &bvh.nodes[current_node_index];
-        let candidate_cost = &candidate.aabb.half_area();
 
-        let mut inherited_cost_delta = 0.0f32; // The increased cost caused by refitting the ancestor's aabbs.
-
-        // traversal_threshold is so we don't bother checking up the tree for really small changes in area.
-        // At some point when traversing up the new node might be mostly already inside the larger inner aabb
-        let traversal_threshold = candidate_cost * 0.01;
-        let mut index = current_node_index;
-        // Work back up the tree accumulating the additional cost of adding this node as a sibling of candidate since
-        // each parent up the tree will also potentially increase in size.
-        loop {
-            let aabb = &bvh.nodes[index].aabb;
-            // How much the cost of this node will increase as a result of adding the new node below it in the hierarchy
-            let cost_delta = aabb.union(&new_node.aabb).half_area() - aabb.half_area();
-            inherited_cost_delta += cost_delta;
-            if index == 0 || inherited_cost_delta < traversal_threshold {
-                break;
-            }
-            index = parents[index] as usize;
-        }
-
-        let direct_cost = new_node.aabb.union(&candidate.aabb).half_area();
-        let cost = direct_cost + inherited_cost_delta;
+        let direct_cost = candidate.aabb.union(&new_node.aabb).half_area();
+        let cost_increase = direct_cost - candidate.aabb.half_area();
+        let inherited_cost = sibling_candidate.inherited_cost + cost_increase;
+        let cost = direct_cost + inherited_cost;
 
         if cost < min_cost {
             min_cost = cost;
             best_sibling_candidate_id = current_node_index;
             // If this is not a leaf, it's possible a better cost could be found further down.
             if !candidate.is_leaf() {
-                for child_idx in [candidate.first_index, candidate.first_index + 1] {
-                    let child = &bvh.nodes[child_idx as usize];
-                    let min_subtree_cost =
-                        new_node.aabb.union(&child.aabb).half_area() + inherited_cost_delta;
-                    if min_subtree_cost < min_cost {
-                        stack.push(child_idx);
-                    }
-                }
+                stack.push(SiblingCandidate {
+                    inherited_cost,
+                    index: candidate.first_index,
+                });
+                stack.push(SiblingCandidate {
+                    inherited_cost,
+                    index: candidate.first_index + 1,
+                });
             }
         }
     }
