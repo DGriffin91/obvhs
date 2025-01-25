b7dfb6e2e213d4f1dbad49f06dc4b9c6e162ad71
DGriffin91 <github@dgdigital.net>
2025-01-25 12:26:38 -0800

    docs, better/simpler sub tree cost heuristic

--- src/bvh2/insertion_removal.rs
+++ src/bvh2/insertion_removal.rs
@@ -17,33 +17,43 @@ pub fn remove_node(bvh: &mut Bvh2, node_id: usize, parents: &[u32]) {
     bvh.refit_from_fast(parent_id, &parents);
 }
 
-// See "Branch and Bound" https://box2d.org/files/ErinCatto_DynamicBVH_Full.pdf
+/// Searches through tree recursively to find the best sibling for the node being inserted. The best sibling is
+/// classified as the sibling that if chosen would increase the surface area of the BVH the least.
+/// When the best sibling is found, a parent of both the sibling and the new node in put in the location of
+/// the sibling and both the sibling and new node are added to the end of the bvh.nodes.
+/// See "Branch and Bound" https://box2d.org/files/ErinCatto_DynamicBVH_Full.pdf
 pub fn insert_leaf_node(
     bvh: &mut Bvh2,
-    node: Bvh2Node,
+    new_node: Bvh2Node,
     parents: &mut Vec<u32>,
     stack: &mut HeapStack<u32>,
 ) {
-    debug_assert!(node.is_leaf());
+    debug_assert!(new_node.is_leaf());
     let mut min_cost = f32::MAX;
     let mut best_sibling_candidate_id = 0;
 
     stack.clear();
+
+    // Traverse the BVH to find the best sibling
     stack.push(0);
     while let Some(current_node_index) = stack.pop() {
         let current_node_index = *current_node_index as usize;
 
-        let candidate = bvh.nodes[current_node_index];
+        let candidate = &bvh.nodes[current_node_index];
         let candidate_cost = &candidate.aabb.half_area();
 
-        let mut inherited_cost_delta = 0.0f32;
+        let mut inherited_cost_delta = 0.0f32; // The increased cost caused by refitting the ancestor's aabbs.
 
         // traversal_threshold is so we don't bother checking up the tree for really small changes in area.
+        // At some point when traversing up the new node might be mostly already inside the larger inner aabb
         let traversal_threshold = candidate_cost * 0.01;
         let mut index = current_node_index;
+        // Work back up the tree accumulating the additional cost of adding this node as a sibling of candidate since
+        // each parent up the tree will also potentially increase in size.
         loop {
             let aabb = &bvh.nodes[index].aabb;
-            let cost_delta = aabb.union(&node.aabb).half_area() - aabb.half_area();
+            // How much the cost of this node will increase as a result of adding the new node below it in the hierarchy
+            let cost_delta = aabb.union(&new_node.aabb).half_area() - aabb.half_area();
             inherited_cost_delta += cost_delta;
             if index == 0 || inherited_cost_delta < traversal_threshold {
                 break;
@@ -51,45 +61,48 @@ pub fn insert_leaf_node(
             index = parents[index] as usize;
         }
 
-        let join_cost = node.aabb.union(&candidate.aabb).half_area();
-        let cost = join_cost + inherited_cost_delta;
-        let min_sub_tree_cost = candidate_cost + inherited_cost_delta;
-
-        // If this is not a leaf, and it's possible a better cost could be found further down
-        if !candidate.is_leaf() && min_sub_tree_cost < min_cost {
-            stack.push(candidate.first_index);
-            stack.push(candidate.first_index + 1);
-        }
+        let direct_cost = new_node.aabb.union(&candidate.aabb).half_area();
+        let cost = direct_cost + inherited_cost_delta;
 
         if cost < min_cost {
+            // If this is not a leaf, it's possible a better cost could be found further down.
+            if !candidate.is_leaf() {
+                stack.push(candidate.first_index);
+                stack.push(candidate.first_index + 1);
+            }
             min_cost = cost;
             best_sibling_candidate_id = current_node_index;
         }
     }
 
     let best_sibling_candidate = bvh.nodes[best_sibling_candidate_id];
-    let sibling_is_inner = !best_sibling_candidate.is_leaf();
 
     // To avoid having gaps or re-arranging the BVH:
-    // The new parent goes in the sibling's position
+    // The new parent goes in the sibling's position.
     // The sibling and new node go on the end.
+    let new_sibling_id = bvh.nodes.len() as u32;
     let new_parent = Bvh2Node {
-        aabb: node.aabb.union(&best_sibling_candidate.aabb),
+        aabb: new_node.aabb.union(&best_sibling_candidate.aabb),
         prim_count: 0,
-        first_index: bvh.nodes.len() as u32,
+        first_index: new_sibling_id,
     };
 
-    bvh.nodes[best_sibling_candidate_id] = new_parent;
-    if sibling_is_inner {
-        let new_sibling_id = bvh.nodes.len() as u32;
+    // New parent goes in the sibling's position.
+    let new_parent_id = best_sibling_candidate_id;
+    bvh.nodes[new_parent_id] = new_parent;
+
+    // If the best selected sibling was an inner node, we need to update the parents mapping so that the children of
+    // that node point to the new location that it's being moved to.
+    if !best_sibling_candidate.is_leaf() {
         parents[best_sibling_candidate.first_index as usize] = new_sibling_id;
         parents[best_sibling_candidate.first_index as usize + 1] = new_sibling_id;
     }
     bvh.nodes.push(best_sibling_candidate);
-    bvh.nodes.push(node);
-    parents.push(best_sibling_candidate_id as u32);
-    parents.push(best_sibling_candidate_id as u32);
-    bvh.refit_from(best_sibling_candidate_id, &parents);
+    bvh.nodes.push(new_node);
+    parents.push(new_parent_id as u32);
+    parents.push(new_parent_id as u32);
+    // Need to work up the tree updating the aabbs since we just added a node.
+    bvh.refit_from_fast(new_parent_id, &parents);
 }
 
 /// Slow at building, makes a slow bvh, just for testing insertion.
